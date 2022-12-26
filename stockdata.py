d816c6cc52300964ed02c2f8f8cd78f6c6c3a7df
Mantvydas <77936465+Mantvydas-data@users.noreply.github.com>
2022-12-26 17:47:51 +0000

    Update stockdata.py
    API key removed

--- stockdata.py
+++ stockdata.py
@@ -8,7 +8,7 @@ def stockhistory(ticker):
     querystring = {"symbol":ticker,"interval":"15m","diffandsplits":"false"}
 
     headers = {
-	    "X-RapidAPI-Key": "cb13e099f6msh76bac0554262195p1e57e4jsnec3cb7675f2c",
+	    "X-RapidAPI-Key": "apikey",
 	    "X-RapidAPI-Host": "mboum-finance.p.rapidapi.com"
     }
 
